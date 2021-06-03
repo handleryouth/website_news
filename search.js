41c236815b35d040b83b6ba38a088f88bf7b28cc
handleryouth <rafaeltonydavid@yahoo.com>
2021-06-03 23:46:16 +0700

    fix bugs

--- search.js
+++ search.js
@@ -1,19 +1,21 @@
 //jshint esversion:6
 
-
+const express = require('express');
+const app = express();
 const https = require("https");
+
+var customHeaderList = [];
+var customDescriptionList = [];
+var customImagesList = [];
 
 
 module.exports.customSearch = function(req, res, query) {
-  let customURL = "https://newsapi.org/v2/everything?q=" + query + "&apiKey=1c13c715ec12463cbd624e2005e0f66f&pageSize=10";
-
-  var customHeaderList = [];
-  var customDescriptionList = [];
-  var customImagesList = [];
+  let customURL = "https://newsapi.org/v2/everything?q=" + query + "&apiKey=1c13c715ec12463cbd624e2005e0f66f";
 
 
   https.get(customURL, function(response) {
     console.log(response.statusCode);
+    console.log(customURL);
     let chunks = "";
     response.on('data', function(data) {
       chunks += data;
@@ -21,7 +23,9 @@ module.exports.customSearch = function(req, res, query) {
 
     response.on("end", function() {
       let News = JSON.parse(chunks);
-      if (News.totalResults <= 10) {
+      console.log(News);
+      console.log(News.totalResults);
+      if (News.totalResults <= 10 && News.totalResults >= 1) {
         for (let i = 0; i < News.totalResults; i++) {
           let customNewsHeader = News.articles[i].title;
           let customNewsDescription = News.articles[i].description;
@@ -30,30 +34,32 @@ module.exports.customSearch = function(req, res, query) {
           customDescriptionList.push(customNewsDescription);
           customImagesList.push(customNewsImage);
         }
+      }
+        else if(News.totalResults > 10){
+          for (let i = 0; i < 10; i++) {
+            let customNewsHeader = News.articles[i].title;
+            let customNewsDescription = News.articles[i].description;
+            let customNewsImage = News.articles[i].urlToImage;
+            customHeaderList.push(customNewsHeader);
+            customDescriptionList.push(customNewsDescription);
+            customImagesList.push(customNewsImage);
+          }
+        }
 
-        setTimeout(function(){
-          res.render("search.ejs", {
-            customHeader : customHeaderList,
-            customDescription : customDescriptionList,
-            customImages : customImagesList
-          });
-        }, 10000);
-
+      else if (News.totalResults === 0) {
+        customHeaderList.push("Nothing, sorry.");
       }
 
-      else if(News.totalResults === 0){
-        customHeaderList.push("Nothing, sorry :(");
-        setTimeout(function(){
-          res.render("search.ejs", {
-            customHeader : customHeaderList,
-            customDescription : customDescriptionList,
-            customImages : customImagesList
-          });
-        }, 10000);
-
-      }
-
+      setTimeout(function() {
+        res.render("search.ejs", {
+          customHeader: customHeaderList,
+          customDescription: customDescriptionList,
+          customImages: customImagesList
+        });
+      }, 10000);
 
     });
   });
+
+
 };
